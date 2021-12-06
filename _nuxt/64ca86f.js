6a63a065303e3a036f4eba817f7360dcf0d96a14
Webxcompanygroup <89280531+Webxcompanygroup@users.noreply.github.com>
2021-12-06 03:15:01 +0300

    add

---
+++ _nuxt/64ca86f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{312:function(n,e,t){"use strict";t.r(e);var l=t(16),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("SSR dApp")])}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1638745788/users/4/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fssr-users\u002F_nuxt\u002Fstatic\u002F1638745788",layout:"default",error:a,state:{users:{users:[],gallery:[]}},serverRendered:true,routePath:"\u002Fusers\u002F4",config:{_app:{basePath:"\u002Fssr-users\u002F",assetsPath:"\u002Fssr-users\u002F_nuxt\u002F",cdnURL:a}}}}(null));
